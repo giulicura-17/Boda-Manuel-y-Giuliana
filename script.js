5c3240fef2e50e733d47a7d7e030b3d883550b52
giulicura-17 <giulicura@gmail.com>
2026-07-13 13:04:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+if("scrollRestoration" in history){history.scrollRestoration="manual";}
+window.addEventListener("load",()=>window.scrollTo(0,0));
 if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
 window.addEventListener('pageshow', () => {
   if (!window.location.hash) window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
