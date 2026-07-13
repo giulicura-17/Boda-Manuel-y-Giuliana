fd061a10231a9e768d44a48baf20efcd9ce0022b
giulicura-17 <giulicura@gmail.com>
2026-07-13 11:03:43 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 */
 window.WEDDING_CONFIG = {
   pinterestUrl: "https://ar.pinterest.com/giulycura/dress-code-inspo/",
-  formsUrl: "PEGAR_AQUI_LINK_DE_GOOGLE_FORMS",
+  formsUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe9uPfirYzEvY1Vii99k3YaNeB2bK-FkzWe0I3OQNAPhqpy0w/viewform?usp=publish-editor",
   mapsUrl: "https://www.google.com/maps/search/?api=1&query=Palacio+Sans+Souci+Paz+705+Victoria+Buenos+Aires",
   weddingDate: "2026-11-06T17:15:00-03:00"
 };
